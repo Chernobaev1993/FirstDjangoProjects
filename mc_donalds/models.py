520d42c90c5d1ad2b14780242fb364b58a624ca2
Aleksandr Chernobaev <chernobaevap1993@gmail.com>
2023-04-28 10:04:31 +0500

    alter tables

--- mc_donalds/models.py
+++ mc_donalds/models.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from django.db import models
+from resources import POSITIONS, cashier
 
 
 class Order(models.Model):
@@ -10,25 +12,21 @@ class Order(models.Model):
     staff = models.ForeignKey('Staff', on_delete=models.CASCADE)
     products = models.ManyToManyField('Product', through='ProductOrder')
 
+    def finish_order(self):
+        self.time_out = datetime.now()
+        self.complete = True
+        self.save()
+
+    def get_duration(self):
+        if self.complete:
+            return (self.time_out - self.time_in).total_seconds() // 60
+        else:
+            return (datetime.now() - self.time_in).total_seconds() // 60
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField(default=0.0)
-
-
-director = 'DI'
-admin = 'AD'
-cook = 'CO'
-cashier = 'CA'
-cleaner = 'CL'
-
-POSITIONS = [
-    (director, 'Директор'),
-    (admin, 'Администратор'),
-    (cook, 'Повар'),
-    (cashier, 'Кассир'),
-    (cleaner, 'Уборщик')
-]
 
 
 class Staff(models.Model):
@@ -40,4 +38,16 @@ class Staff(models.Model):
 class ProductOrder(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    amount = models.IntegerField(default=1)
+    _amount = models.IntegerField(default=1, db_column='amount')
+
+    def product_sum(self):
+        return self.product.price * self.amount
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, value):
+        self._amount = int(value) if value >= 0 else 0
+        self.save()
